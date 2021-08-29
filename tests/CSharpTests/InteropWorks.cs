690f14b6af4d16655a34dfa477ff12c68612dcbb
WhiteBlackGoose <wbg@angouri.org>
2021-08-29 16:38:51 +0300

    Type preservation test

--- tests/CSharpTests/InteropWorks.cs
+++ tests/CSharpTests/InteropWorks.cs
@@ -1,6 +1,9 @@
 using System;
 using Xunit;
 using DynamicObj;
+using Microsoft.FSharp.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpTests
 {
@@ -20,6 +23,22 @@ namespace CSharpTests
                 .With("bb", 10)
                 ;
             Assert.Equal(obj1, obj2);
+        }
+
+        public class MyDynamicObject : ImmutableDynamicObj
+        {
+            public MyDynamicObject(FSharpMap<string, object> map) : base(map) { }
+            public MyDynamicObject() : base(new(Enumerable.Empty<Tuple<string, object>>())) { }
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var obj1 =
+                new MyDynamicObject()
+                .With("aaa", 5);
+            Assert.IsType<MyDynamicObject>(obj1);
+            Assert.Equal(5, obj1["aaa"]);
         }
     }
 }
