d35d978ff06fbbcadb048386def43ed9e6d46ea9
Kevin Schneider <schneider.kev@outlook.de>
2021-09-05 11:21:42 +0200

    Update release notes and docs

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,7 @@
+### 0.2.0 - 09/05/2021
+- [Add `ImmutableDynamicObj`](https://github.com/CSBiology/DynamicObj/pull/4) as the immutable counterpart of `DynamicObj` (thanks [@WhiteBlackGoose](https://github.com/WhiteBlackGoose)) 
+- Add print formatters for both `DynamicObj` and `ImmutableDynamicObj`
+
 ### 0.1.0 - 08/17/2021
 - target netstandard2.0 and .net5.0
 - add custom Equality and GetHashcode (thanks [@WhiteBlackGoose](https://github.com/WhiteBlackGoose)) 
--- docs/index.fsx
+++ docs/index.fsx
@@ -14,11 +14,26 @@
 
 # DynamicObj
 
-F# library supporting Dynamic Objects including inheritance in functional style. It builds on ´System.Dynamic´ but adds object inheritance.
+F# library supporting Dynamic Objects including inheritance in functional style.
+
+#### Table of contents
+
+- [DynamicObj (mutable)](#DynamicObj-mutable)
+    - [Simple inheritance pattern for DynamicObj](#Simple-inheritance-pattern-for-DynamicObj)
+    - [Nesting DynamicObjs](#Nesting-DynamicObjs)
+- [ImmutableDynamicObj](#ImmutableDynamicObj)
+    - [Simple inheritance pattern for ImmutableDynamicObj](#Simple-inheritance-pattern-for-ImmutableDynamicObj)
+    - [Nesting ImmutableDynamicObjs](#Nesting-ImmutableDynamicObjs)
+
+# DynamicObj (mutable)
+
+`DynamicObj` builds on ´System.Dynamic´ but adds object inheritance.
 
 One main use case of this library is the dynamic generation of JSON objects - especially nested objects with optional properties - aimed to be used from javascript wuith the init/style pattern:
 
 Use it for your custom types via inheritance:
+
+## Simple inheritance pattern for DynamicObj
 
 *)
 #r "nuget: Newtonsoft.JSON, 12.0.3"
@@ -48,6 +63,18 @@ type A() =
 
                 a
 
+(**
+You can use the `DynObj.print` function to look at the dynamic members of the object:
+*)
+
+A.init(42) |> DynObj.print
+
+(***include-output***)
+
+(**
+And this is how the serialized JSON looks like:
+*)
+
 
 let aSerialized =
     A.init(42)
@@ -59,6 +86,10 @@ aSerialized
 #endif // IPYNB
 
 (***include-value:aSerialized***)
+
+(**
+## Nesting DynamicObjs
+*)
 
 type MyComplexJSONType() =
     inherit DynamicObj()
@@ -87,12 +118,27 @@ type MyComplexJSONType() =
 
                 t
 
-let complexSerialized =
+let complex =
     MyComplexJSONType.init(
         PropA = [|42;1337|],
         PropB = A.init(68) // nested dynamic objects
     )
+
+let complexSerialized = 
+    complex
     |> JsonConvert.SerializeObject
+
+(**
+You can use the `DynObj.print` function to look at the dynamic members of the object:
+*)
+
+complex |> DynObj.print
+
+(***include-output***)
+
+(**
+And this is how the serialized JSON looks like:
+*)
 
 (*** condition: ipynb ***)
 #if IPYNB
@@ -100,3 +146,127 @@ complexSerialized
 #endif // IPYNB
 
 (***include-value:complexSerialized***)
+
+(**
+
+*)
+
+(**
+## Simple inheritance pattern for ImmutableDynamicObj
+
+*)
+#r "nuget: Newtonsoft.JSON, 12.0.3"
+open Newtonsoft.Json
+open DynamicObj
+
+type ImmutableA() = 
+    inherit ImmutableDynamicObj()
+
+    static member init
+        (
+            ?SomeProp: int
+        ) =
+            ImmutableA()
+            |> ImmutableA.style
+                (
+                    ?SomeProp  = SomeProp
+                )
+
+    static member style
+        (
+            ?SomeProp: int
+        ) =
+            fun (a:ImmutableA) ->
+                a 
+                |> ImmutableDynamicObj.addOpt "some_prop" SomeProp
+
+
+(**
+You can use the `ImmutableDynamicObj.print` function to look at the dynamic members of the object:
+*)
+
+ImmutableA.init(42) |> ImmutableDynamicObj.print
+
+(***include-output***)
+
+(**
+And this is how the serialized JSON looks like:
+*)
+
+
+let immutableASerialized =
+    ImmutableA.init(42)
+    |> JsonConvert.SerializeObject
+
+(*** condition: ipynb ***)
+#if IPYNB
+immutableASerialized
+#endif // IPYNB
+
+(***include-value:immutableASerialized***)
+
+(**
+## Nesting DynamicObjs
+
+`DynamicObj.Operators` adds usefull operators for adding properties:
+*)
+
+open DynamicObj.Operators
+
+type ImmutableMyComplexJSONType() =
+    inherit ImmutableDynamicObj()
+
+    static member init
+        (
+            ?PropA: int [],
+            ?PropB: A
+        ) =
+            ImmutableMyComplexJSONType()
+            |> ImmutableMyComplexJSONType.style
+                (
+                    ?PropA  = PropA,
+                    ?PropB  = PropB
+                )
+
+    static member style
+        (
+            ?PropA: int [],
+            ?PropB: A
+        ) =
+            fun (t:ImmutableMyComplexJSONType) ->
+                t
+                ++? ("prop_a", PropA)
+                ++? ("prop_b", PropB)
+
+let immutableComplex =
+    ImmutableMyComplexJSONType.init(
+        PropA = [|42;1337|],
+        PropB = A.init(68) // nested dynamic objects
+    )
+
+let immutableComplexSerialized = 
+    immutableComplex
+    |> JsonConvert.SerializeObject
+
+(**
+You can use the `DynObj.print` function to look at the dynamic members of the object:
+*)
+
+complex |> DynObj.print
+
+(***include-output***)
+
+(**
+And this is how the serialized JSON looks like:
+*)
+
+(*** condition: ipynb ***)
+#if IPYNB
+immutableComplexSerialized
+#endif // IPYNB
+
+(***include-value:immutableComplexSerialized***)
+
+(**
+
+*)
