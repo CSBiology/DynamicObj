2ae037c6d20d201f6fbd10033573a685606d87bd
Kevin F <Freymaurer@gmx.de>
2022-04-25 21:10:35 +0200

    Add Json to DynamicObj converter

--- src/DynamicObj/DynamicObj.fs
+++ src/DynamicObj/DynamicObj.fs
@@ -1,8 +1,9 @@
-﻿namespace DynamicObj
+﻿namespace rec DynamicObj
 
 open System.Dynamic
 open System.Collections.Generic
 
+open Newtonsoft.Json
 
 type DynamicObj internal (dict:Dictionary<string, obj>) = 
     
@@ -136,3 +137,128 @@ type DynamicObj internal (dict:Dictionary<string, obj>) =
         |> List.sortBy (fun pair -> pair.Key)
         |> List.map (fun pair -> struct (pair.Key, pair.Value))
         |> (fun l -> l.GetHashCode())
+
+    member this.ToJson () = JsonConvert.SerializeObject(this, new DynamicObjConverter())
+
+
+module DynamicObj =
+
+    let toJson (dynObj:DynamicObj) = JsonConvert.SerializeObject(dynObj, new DynamicObjConverter())
+
+    /// Read json string to DynamicObj. If json string has an array as root element the array will be set as value with "root" as key.
+    let ofJson (jsonSource:string) = JsonConvert.DeserializeObject<DynamicObj>(jsonSource, new DynamicObjConverter())
+
+
+type private DynamicObjConverter() =
+    inherit JsonConverter<DynamicObj>()
+
+    override this.ReadJson(reader : JsonReader, objectType : System.Type, existingValue : DynamicObj, hasExistingValue:bool, serializer : JsonSerializer) : DynamicObj = 
+       
+        /// The isInit parameter is necessary as the reader starts with the first value.
+        /// But every iteration thereafter we need to progress the reader to the next value, with reader.next().
+        let rec readJsonParserFieldToDynObj (result: obj option) (isInit:bool) =
+            let addValueToParentList(listObj:obj option) (value:'a) =
+                /// unbox 'a does not seem to provide any benefit. When comparing output to manually created dyn object,
+                /// it still needs to be boxed to be equal.
+                let list = listObj.Value :?> obj seq |> Seq.map (fun x -> unbox<'a> x) |> List.ofSeq
+                let res = (value::list) |> Seq.ofList |> box
+                readJsonParserFieldToDynObj (Some res) false
+            let next = isInit || reader.Read()
+            if next = false then 
+                result
+            else 
+                let isList = result.IsSome && result.Value :? obj seq
+                let tokenType = reader.TokenType
+                let tokenValue = (if isNull reader.Value then None else string reader.Value |> Some)
+                //printfn "%A, %A" tokenType tokenValue
+                match tokenType with
+                | JsonToken.StartObject ->
+                    let obj = DynamicObj() |> box
+                    if isList then
+                        let v = readJsonParserFieldToDynObj (Some obj) false
+                        addValueToParentList result v.Value
+                    else
+                        readJsonParserFieldToDynObj (Some obj) false
+                | JsonToken.EndObject -> 
+                    result
+                | JsonToken.StartArray ->
+                    /// Need to use Sequence to be able to use any casting to and from: obj seq <-> 'a seq
+                    let list: obj seq = Seq.empty
+                    readJsonParserFieldToDynObj (Some <| box list) false
+                | JsonToken.EndArray ->
+                    let list = result.Value :?> obj seq |> List.ofSeq |> List.rev
+                    Some <| box list
+                | JsonToken.PropertyName ->
+                    let key = tokenValue.Value
+                    if result.IsNone then failwith "Cannot apply property without parent dyn object."
+                    let parent = 
+                        match result.Value with
+                        | :? DynamicObj ->
+                            let logger = result.Value :?> DynamicObj
+                            let v = readJsonParserFieldToDynObj None false
+                            logger.SetValue(key, v.Value)
+                            logger |> box
+                        | _ -> failwith "Cannot parse parent type to supported types." 
+                    readJsonParserFieldToDynObj (Some parent) false
+                | JsonToken.String -> 
+                    let v = string tokenValue.Value
+                    if isList then
+                        addValueToParentList result v
+                    else
+                        Some <| box v
+                | JsonToken.Integer -> 
+                    let v = int tokenValue.Value
+                    if isList then
+                        addValueToParentList result v
+                    else
+                        Some <| box v
+                | JsonToken.Float -> 
+                    let v = float tokenValue.Value
+                    if isList then
+                        addValueToParentList result v
+                    else
+                        Some <| box v
+                | JsonToken.Boolean ->
+                    let v = System.Boolean.Parse tokenValue.Value
+                    if isList then
+                        addValueToParentList result v
+                    else
+                        Some <| box v
+                | JsonToken.Null ->
+                    let v = None
+                    if isList then
+                        addValueToParentList result v
+                    else
+                        Some <| box v
+                | JsonToken.Bytes | JsonToken.Date ->
+                    let v = string tokenValue.Value
+                    if isList then
+                        addValueToParentList result v
+                    else
+                        Some <| box v
+                | any -> 
+                    readJsonParserFieldToDynObj None false
+        let res = readJsonParserFieldToDynObj(None) true |> Option.get
+        match res with
+        | :? list<obj> as list ->
+            let loggerList = list
+            let r = DynamicObj()
+            r.SetValue("root", loggerList)
+            r
+        | :? DynamicObj as root ->
+            root
+        | _ -> failwith "Could not parse Result to any supported type."
+
+    override this.WriteJson(writer : JsonWriter, value : DynamicObj, serializer : JsonSerializer) =
+        let v =
+            let settings = 
+                let s = JsonSerializerSettings()
+                s.ReferenceLoopHandling <- ReferenceLoopHandling.Serialize
+                s
+            let hasRootArr = value.TryGetValue "root"
+            if hasRootArr.IsSome then
+                hasRootArr.Value 
+                |> fun v -> JsonConvert.SerializeObject(v, settings)
+            else
+                JsonConvert.SerializeObject(value, settings)
+        writer.WriteRaw (v)
