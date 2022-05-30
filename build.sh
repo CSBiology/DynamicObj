c45361cc26e0bf164e790c8dd9da6e76cc333c47
Kevin Schneider <schneider.kev@outlook.de>
2022-05-30 10:52:06 +0200

    Update to project-based build pipeline

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 set -eu
 set -o pipefail
 
-dotnet tool restore
-dotnet fake "$@"
+dotnet run --project ./build/build.fsproj "$@"
---
+++ build/ProjectInfo.fs
@@ -0,0 +1,35 @@
+﻿module ProjectInfo
+
+open Fake.Core
+
+let project = "DynamicObj"
+
+let testProjects = 
+    [
+        "tests/UnitTests/UnitTests.fsproj"
+        "tests/CSharpTests/CSharpTests.csproj"
+    ]
+
+let solutionFile  = $"{project}.sln"
+
+let configuration = "Release"
+
+let gitOwner = "CSBiology"
+
+let gitHome = $"https://github.com/{gitOwner}"
+
+let projectRepo = $"https://github.com/{gitOwner}/{project}"
+
+let pkgDir = "pkg"
+
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let stableVersion = SemVer.parse release.NugetVersion
+
+let stableVersionTag = (sprintf "%i.%i.%i" stableVersion.Major stableVersion.Minor stableVersion.Patch )
+
+let mutable prereleaseSuffix = ""
+
+let mutable prereleaseTag = ""
+
+let mutable isPrerelease = false
