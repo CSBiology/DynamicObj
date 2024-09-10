dc46ef793dfb3eb7d5280a40e018333cfd278f86
Kevin Schneider <schneider.kev@outlook.de>
2024-09-10 08:38:05 +0200

    Split Instance methods and DynObj tests

--- tests/DynamicObject.Tests/InstanceMethods.fs
+++ tests/DynamicObject.Tests/InstanceMethods.fs
@@ -1,10 +1,12 @@
-﻿module DynamicObj.Tests
+﻿module InstanceMethods.Tests
 
 open System
 open Fable.Pyxpecto
 open DynamicObj
 
-let tests_tryGetTypedValue = testList "TryGetTypedValue" [
+#if !FABLE_COMPILER
+// instance method TryGetTypedValue is not Fable-compatible
+let tests_TryGetTypedValue = testList "TryGetTypedValue" [
     
     testCase "typeof" <| fun _ -> 
         let a = typeof<int>
@@ -12,61 +14,61 @@ let tests_tryGetTypedValue = testList "TryGetTypedValue" [
 
     testCase "NonExisting" <| fun _ -> 
         let a = DynamicObj()
-        let b = DynObj.tryGetTypedValue<int> "a" a
+        let b = a.TryGetTypedValue<int> "a"
         Expect.isNone b "Value should not exist"
 
     testCase "Correct Int" <| fun _ -> 
         let a = DynamicObj()
         a.SetValue("a", 1)
-        let b = DynObj.tryGetTypedValue<int> "a" a
+        let b = a.TryGetTypedValue<int> "a"
         Expect.equal b (Some 1) "Value should be 1"
 
     testCase "Incorrect Int" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", "1")
-        let b = DynObj.tryGetTypedValue<int> "a" a
+        let b = a.TryGetTypedValue<int> "a"
         Expect.isNone b "Value should not be an int"
 
     testCase "Correct String" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", "1")
-        let b = DynObj.tryGetTypedValue<string> "a" a
+        let b = a.TryGetTypedValue<string> "a"
         Expect.equal b (Some "1") "Value should be '1'"
 
     testCase "Incorrect String" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", 1)
-        let b = DynObj.tryGetTypedValue<string> "a" a
+        let b = a.TryGetTypedValue<string> "a"
         Expect.isNone b "Value should not be a string"
 
     testCase "Correct List" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", [1; 2; 3])
-        let b = DynObj.tryGetTypedValue<int list> "a" a
+        let b = a.TryGetTypedValue<int list> "a"
         Expect.equal b (Some [1; 2; 3]) "Value should be [1; 2; 3]"
 
     ptestCase "Incorrect List" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", [1; 2; 3])
-        let b = DynObj.tryGetTypedValue<string list> "a" a
+        let b = a.TryGetTypedValue<string list> "a"
         Expect.isNone b "Value should not be a string list"
 
     testCase "Correct DynamicObj" <| fun _ ->
         let a = DynamicObj()
         let b = DynamicObj()
         a.SetValue("a", b)
-        let c = DynObj.tryGetTypedValue<DynamicObj> "a" a
+        let c = a.TryGetTypedValue<DynamicObj> "a"
         Expect.equal c (Some b) "Value should be a DynamicObj"
 
     testCase "Incorrect DynamicObj" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", 1)
-        let b = DynObj.tryGetTypedValue<DynamicObj> "a" a
+        let b = a.TryGetTypedValue<DynamicObj> "a"
         Expect.isNone b "Value should not be a DynamicObj"
 ]
-    
+#endif
 
-let tests_set = testList "Set" [
+let tests_SetValue = testList "SetValue" [
 
     testCase "Same String" <| fun _ ->
         let a = DynamicObj ()
@@ -137,7 +139,7 @@ let tests_set = testList "Set" [
         Expect.equal (a'.GetHashCode()) (b'.GetHashCode()) "New Hash codes should be equal"
     ]
 
-let tests_remove = testList "Remove" [
+let tests_Remove = testList "Remove" [
   
     testCase "Remove" <| fun _ ->
         let a = DynamicObj ()
@@ -225,119 +227,14 @@ let tests_remove = testList "Remove" [
 
 ]
 
-let tests_getProperties = testList "GetProperties" [
-    testCase "GetProperties" <| fun _ ->
+let tests_GetPropertyHelpers = testList "GetPropertyHelpers" [
+    testCase "GetPropertyHelpers" <| fun _ ->
         let a = DynamicObj()
         a.SetValue("a", 1)
         a.SetValue("b", 2)
         let properties = a.GetPropertyHelpers(true)
         let names = properties |> Seq.map (fun p -> p.Name)
         Expect.equal (Seq.toList names) ["a"; "b"] "Should have all properties"
-]
-
-
-let tests_formatString = testList "FormatString" [
-
-    testCase "Format string 1" <| fun _ ->
-        let foo = DynamicObj()
-        let list = [1;2;3;4]
-        foo.SetValue("bar", list)
-        let expected = $"?bar: {list}"
-        Expect.equal (foo |> DynObj.format) expected "Format string 1 failed"
-
-    testCase "Format string 2" <| fun _ ->
-        let foo = DynamicObj()
-        let corgi = "corgi"
-        foo.SetValue("corgi", corgi)
-        let inner = DynamicObj()
-        let baz = "baz"
-        inner.SetValue("bar", baz)
-        foo.SetValue("foo", inner)
-        let expected = $"""?corgi: {corgi}{Environment.NewLine}?foo:{Environment.NewLine}    ?bar: {baz}"""
-        Expect.equal (foo |> DynObj.format) expected "Format string 2 failed"
-
-]
-
-
-let tests_combine = testList "Combine" [
-
-    testCase "Combine flat DOs" <| fun _ ->
-        let target = DynamicObj()
-
-        target.SetValue("target-unique", [42])
-        target.SetValue("will-be-overridden", "WAS_NOT_OVERRIDDEN!")
-
-        let source = DynamicObj()
-
-        source.SetValue("source-unique", [42; 32])
-        source.SetValue("will-be-overridden", "WAS_OVERRIDDEN =)")
-
-        let combined = DynObj.combine target source
-
-        let expected = DynamicObj()
-
-        expected.SetValue("target-unique", [42])
-        expected.SetValue("source-unique", [42; 32])
-        expected.SetValue("will-be-overridden", "WAS_OVERRIDDEN =)")
-
-        Expect.equal expected combined "Combine flat DOs failed"
-
-    testCase "Combine nested DOs" <| fun _ ->
-        let target = DynamicObj()
-
-        target.SetValue("target-unique", 1337)
-        target.SetValue("will-be-overridden", -42)
-        let something2BeCombined = DynamicObj()
-        something2BeCombined.SetValue("inner","I Am")
-        let something2BeOverriden = DynamicObj()
-        something2BeOverriden.SetValue("inner","NOT_OVERRIDDEN")
-        target.SetValue("nested-will-be-combined", something2BeCombined)
-        target.SetValue("nested-will-be-overridden", something2BeOverriden)
-    
-        let source = DynamicObj()
-
-        source.SetValue("source-unique", 69)
-        source.SetValue("will-be-overridden", "WAS_OVERRIDDEN")
-        let alsoSomething2BeCombined = DynamicObj()
-        alsoSomething2BeCombined.SetValue("inner_combined","Complete")
-        source.SetValue("nested-will-be-combined", alsoSomething2BeCombined)
-        source.SetValue("nested-will-be-overridden", "WAS_OVERRIDDEN")
-    
-        let combined = DynObj.combine target source
-    
-        let expected = DynamicObj()
-
-        expected.SetValue("source-unique", 69)
-        expected.SetValue("target-unique", 1337)
-        expected.SetValue("will-be-overridden", "WAS_OVERRIDDEN")
-        expected.SetValue("nested-will-be-overridden", "WAS_OVERRIDDEN")
-        expected.SetValue("nested-will-be-combined", 
-            let inner = DynamicObj()
-            inner.SetValue("inner","I Am")
-            inner.SetValue("inner_combined","Complete")
-            inner
-            )
-
-        Expect.equal expected combined "Combine nested DOs failed"
-]
-
-let tests_print = testList "Print" [
-
-    testCase "Test Print For Issue 14" <| fun _ ->
-        let outer = DynamicObj()
-        let inner = DynamicObj()
-        inner.SetValue("Level", "Information")
-        inner.SetValue("MessageTemplate","{Method} Request at {Path}")
-        outer.SetValue("serilog", inner)
-
-        let print =
-            try 
-                outer |> DynObj.print
-                true
-            with
-                | e -> false
-
-        Expect.isTrue print "Print failed for issue 14"
 ]
 
 let tests_copyDynamicProperties = testList "CopyDynamicProperties" [
@@ -374,11 +271,14 @@ let tests_copyDynamicProperties = testList "CopyDynamicProperties" [
         Expect.equal a b "Values should be equal"
 ]
 
-let main = testList "DynamicObj" [
-    tests_tryGetTypedValue
-    tests_set
-    tests_remove
-    tests_formatString
-    tests_combine
+let main = testList "Instance Methods" [
+
+    #if !FABLE_COMPILER
+    // instance method TryGetTypedValue is not Fable-compatible
+    tests_TryGetTypedValue
+    #endif
+
+    tests_SetValue
+    tests_Remove
     tests_copyDynamicProperties
 ]
